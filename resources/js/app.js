77e0cabf60f60c032b94a7a369052fc9a25210c5
Maulana Yusuf <maulanayusuf.ena@gmail.com>
2024-03-21 08:01:54 +0700

    add vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,4 +17,17 @@ const app = createApp({
 const pinia = createPinia()
 app.use(pinia)
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(vuetify)
+
 app.mount('#app');
