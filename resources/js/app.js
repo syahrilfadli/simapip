08ed847d81396897d9905cc844d7514e85f8c3be
Junaidi Bardadi <riel.msf28@gmail.com>
2024-04-18 16:10:44 +0700

    adjust: menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import './bootstrap';
 
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import { createPinia } from 'pinia'
-import { SomeComponent } from "@ntohq/buefy-next";
 import api from './api.js';
 
 import Buefy from '@ntohq/buefy-next';
